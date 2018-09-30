4b66dc9bcfaff6e591b96f2b904472f737748fdd
Glitch (hello-express) <none>
2018-09-30 09:18:26 +0000

    👂🐡 Checkpoint ./server.js:1430797/251

--- server.js
+++ server.js
@@ -2,16 +2,13 @@
 var express = require('express');
 var proxy = require('http-proxy-middleware');
 
+
+
 // proxy middleware options
 var options = {
-        target: 'https://znw-web.glitch.me', // target host
+        target: 'https://glitch-target.glitch.me', // target host
         changeOrigin: true,               // needed for virtual hosted sites
-        ws: true,                         // proxy websockets
-        router: {
-            // when request.headers.host == 'dev.localhost:3000',
-            // override target 'http://www.example.org' to 'http://localhost:8000'
-            '/' : 'https://znw-web.glitch.me'
-        }
+        ws: true                    // proxy websockets
     };
 
 // create the proxy (without context)
